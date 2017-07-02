37674d82839ad2c0178c4e111a5f540eb59f59e6
Stefano Fogarollo <sirfoga@protonmail.com>
2017-07-02 14:03:03 +0200

    added piklor library

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+window.addEventListener("load", function () {
+    var pk = new Piklor(".color-picker", [
+            "#1abc9c"
+          , "#2ecc71"
+          , "#3498db"
+          , "#9b59b6"
+          , "#34495e"
+          , "#16a085"
+          , "#27ae60"
+          , "#2980b9"
+          , "#8e44ad"
+          , "#2c3e50"
+          , "#f1c40f"
+          , "#e67e22"
+          , "#e74c3c"
+          , "#ecf0f1"
+          , "#95a5a6"
+          , "#f39c12"
+          , "#d35400"
+          , "#c0392b"
+          , "#bdc3c7"
+          , "#7f8c8d"
+        ], {
+            open: ".picker-wrapper .btn"
+        })
+      ;
+
+    pk.colorChosen(function (col) {
+        console.log(col);
+    });
+});
+
 function setImageWithColors(frameColor, aeroColor, stickersColor) {
 	imageUrl = getPathImageWithColors(frameColor, aeroColor, stickersColor);
 
