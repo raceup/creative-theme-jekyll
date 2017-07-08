1d29b00eb0d38dcaa926e01b29563c263404d487
Stefano Fogarollo <sirfoga@protonmail.com>
2017-07-08 10:53:07 +0200

    Fixed color pickers closing

--- js/handlers.js
+++ js/handlers.js
@@ -23,39 +23,51 @@ var frameColor = "c";
 var aeroColor = "c";
 
 window.addEventListener("load", function () {
-    var pkFrame = new Piklor(".frame-color-picker",
+  var pkFrame = new Piklor(".frame-color-picker",
       Object.keys(frameColors),
       {
         open: ".frame-wrapper .btn"
       }
-    );
-
-    pkFrame.colorChosen(function (col) {
-        frameColor = frameColors[col];
-        setImageWithColors(stickersColor, frameColor, aeroColor);
-    });
+  );
 
   var pkAero = new Piklor(".aero-color-picker",
       Object.keys(aeroColors),
       {
         open: ".aero-wrapper .btn"
       }
-    );
-
-    pkAero.colorChosen(function (col) {
-        aeroColor = aeroColors[col];
-        setImageWithColors(stickersColor, frameColor, aeroColor);
-    });
+  );
 
   var pkStickers = new Piklor(".stickers-color-picker",
       Object.keys(stickersColors),
       {
         open: ".stickers-wrapper .btn"
       }
-    );
+  );
 
-    pkStickers.colorChosen(function (col) {
-        stickersColor = stickersColors[col];
-        setImageWithColors(stickersColor, frameColor, aeroColor);
-    });
+  pkFrame.colorChosen(function (col) {
+      frameColor = frameColors[col];
+      setImageWithColors(stickersColor, frameColor, aeroColor);
+
+      pkFrame.close();  /* close all dialogs */
+      pkAero.close();
+      pkStickers.close();
+  });
+
+  pkAero.colorChosen(function (col) {
+      aeroColor = aeroColors[col];
+      setImageWithColors(stickersColor, frameColor, aeroColor);
+
+      pkFrame.close();  /* close all dialogs */
+      pkAero.close();
+      pkStickers.close();
+  });
+
+  pkStickers.colorChosen(function (col) {
+      stickersColor = stickersColors[col];
+      setImageWithColors(stickersColor, frameColor, aeroColor);
+
+      pkFrame.close();  /* close all dialogs */
+      pkAero.close();
+      pkStickers.close();
+  });
 });
